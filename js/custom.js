ef89ba3b22d0cae0feaf2b17d8c9b5237c8215cc
Cheese <33181649+adhesivecheese@users.noreply.github.com>
2022-12-18 15:33:43 -0500

    Update custom.js
    before -> until, after -> since

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ function loadParams() {
 	const urlParams = new URLSearchParams(window.location.search).entries();
 	for(const param of urlParams) {
 		try{
-			if (param[0] == "before" || param[0] == "after") {
+			if (param[0] == "until" || param[0] == "since") {
 				value = new Date(param[1]*1000)
 				offset = new Date().getTimezoneOffset()*60000
 				value = new Date(value - offset).toISOString().slice(0,-1)
@@ -31,18 +31,18 @@ async function load(url) {
 	return obj
 }
 
-function fetchMore(before) {
-	getFromPS(form, before)
+function fetchMore(until) {
+	getFromPS(form, until)
 }
 
 function jsonConverter(data, renderMD, showthumbnails) {
 	count = 0
 	html = ""
-	before = 2147483647
+	until = 2147483647
 	data.forEach(obj => {
 		count += 1
 		display = true
-		before = obj.created_utc
+		until = obj.created_utc
 		timestamp = new Date(obj.created_utc * 1000)
 		timestamp = timestamp.toString().split(" (")[0]
 		thumbnail = ""
@@ -112,16 +112,16 @@ function jsonConverter(data, renderMD, showthumbnails) {
 			`
 			}
 	});
-	if (count > 0){	html += `<input type="submit" class="button is-danger is-fullwidth" value="Fetch More" id="fetch-${before}" onclick="fetchMore(${before})">`}
+	if (count > 0){	html += `<input type="submit" class="button is-danger is-fullwidth" value="Fetch More" id="fetch-${until}" onclick="fetchMore(${until})">`}
 	return html
 }
 
-function getFromPS(form, before=-1){
+function getFromPS(form, until=-1){
 	button = document.getElementById("searchButton")
 	button.value = "Searching..."
-	if (before == -1){ document.getElementById("results").innerHTML="" }
+	if (until == -1){ document.getElementById("results").innerHTML="" }
 	else {
-		moreButton = document.getElementById("fetch-"+before)
+		moreButton = document.getElementById("fetch-"+until)
 		moreButton.value = "Fetching..."
 	}
 	path = "?"
@@ -144,17 +144,17 @@ function getFromPS(form, before=-1){
 		psURL += "&score=" + form.elements['score'].value
 		path  += "&score=" + form.elements['score'].value
 	}
-	if (form.elements['after'].value != '') {
-		after = new Date(form.elements['after'].value).valueOf() / 1000
-		psURL += "&after=" + after
-		path  += "&after=" + after
+	if (form.elements['since'].value != '') {
+		since = new Date(form.elements['since'].value).valueOf() / 1000
+		psURL += "&since=" + since
+		path  += "&since=" + since
 	}
-	if (before != -1) {
-		psURL += "&before=" + before
-	} else if (form.elements['before'].value != '') {
-			before = new Date(form.elements['before'].value).valueOf() / 1000
-			psURL += "&before=" + before
-			path += "&before=" + before
+	if (until != -1) {
+		psURL += "&until=" + until
+	} else if (form.elements['until'].value != '') {
+			until = new Date(form.elements['until'].value).valueOf() / 1000
+			psURL += "&until=" + until
+			path += "&until=" + until
 	}
 	if (form.elements['q'].value != '') {
 		psURL += "&q=" + encodeURIComponent(form.elements['q'].value)
@@ -194,7 +194,7 @@ function getFromPS(form, before=-1){
 
 			document.getElementById("apiInfo").innerHTML = Object.keys(value.data).length + ` Results - <a href='${psURL}'>Generated API URL</a>`
 			button.value = "Search"
-			try { document.getElementById("fetch-"+before).remove() }
+			try { document.getElementById("fetch-"+until).remove() }
 			catch {}
 
 			var list = document.querySelectorAll(".markdown a");
